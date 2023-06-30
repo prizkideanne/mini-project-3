07cde10ce9a730e7fae0ae62ec54b2653cf94c31
prizkideanne <prizkideanne@gmail.com>
2023-06-30 14:39:10 +0700

    alter add column database

---
+++ projects/server/migrations/20230630072639-alter-add-column-storename-users.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("Users", "storeName", {
+      type: Sequelize.STRING(255),
+      after: "phoneNumber",
+      allowNull: false,
+      unique: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("Users", "storeName")
+  },
+};
