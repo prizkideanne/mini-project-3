9b1255ea51cec9d17464e6d2b96722b759f56610
fabianprastyad <fabian123pd@gmail.com>
2023-07-07 21:12:39 +0700

    FE responsive and axios category for create and modify product

--- projects/client/src/pages/ProductForm.jsx
+++ projects/client/src/pages/ProductForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import InputWithValidation from "../components/InputWithValidation";
@@ -9,6 +9,7 @@ import withAuth from "../withAuth";
 function ProductForm() {
   const [errorMessage, setErrorMessage] = useState("");
   const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState([]);
   const navigate = useNavigate();
 
   const token = localStorage.getItem("token");
@@ -28,6 +29,19 @@ function ProductForm() {
     },
   });
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/category/getAllCategories")
+      .then((response) => {
+        setCategories(response.data.data);
+        console.log(response.data)
+        console.log(categories)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   const handleOnCreateproduct = (values) => {
     const { name, price, description, category } = values;
     console.log("productForm", values);
@@ -35,7 +49,7 @@ function ProductForm() {
     data.append("name", name);
     data.append("price", price);
     data.append("description", description);
-    data.append("categoryId", category);
+    data.append("category", category);
     data.append("file", file);
     axios
       .post("http://localhost:8000/product/createProduct", data, {
@@ -77,7 +91,7 @@ function ProductForm() {
 
           {/* tombol pilih gambar */}
           <label
-            className="text-slate-100 mb-9 w-full text-white bg-indigo-500 bg-contain text-center"
+            className="cursor-pointer text-slate-100 mb-9 w-full text-white bg-indigo-500 bg-contain text-center"
             htmlFor="fileinput"
           >
             Choose Photo Product
@@ -115,58 +129,33 @@ function ProductForm() {
           <textarea
             name="description"
             placeholder="description"
-            className="border-1 rounded-md border  border-black"
+            className="resize border-1 rounded-md border  border-black"
             values={formik.values.description}
             onChange={formik.handleChange}
           ></textarea>
 
           <span>Category</span>
-          <InputWithValidation
-            formikConfig={formik.getFieldProps("category")}
-            name="category"
-            placeholder="category"
-            touched={formik.touched.category}
-            error={formik.errors.category}
-          />
 
-          <button type="submit" className="text-slate-100	bg-indigo-500 text-white" >
+          <select
+            className="mb-5 ml-2 bg-gray-200 outline-none border-rounded"
+            onChange={formik.handleChange}
+            name="Category"
+          >
+            {categories.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+
+
+          <button type="submit" className="cursor-pointer text-slate-100	bg-indigo-500 text-white" >
             Publish
           </button>
         </form>
       </div>
     </div>
-    // <Formik
 
-    //   onSubmit={(e) => {
-    //     e.preventDefault();
-    //     formik.handleSubmit();
-    //   }}
-    // >{props => (
-
-    //   <form onSubmit={props.handleSubmit} className='flex flex-col justify-center item-center h-screen w-screen'>
-    //     <div className='mb-9 flex justify-center'>
-    //       <img className="box-border h-40 w-40 p-4 border-4 item" src="https://www.blibli.com/friends-backend/wp-content/uploads/2022/08/5-Ciri-Sepatu-Warrior-Asli-Kamu-Wajib-Tahu.jpeg" alt="" />
-    //     </div>
-    //     <label className="bg-contain text-center text-slate-100 bg-indigo-500" htmlFor='inputFile'>
-    //       import photo product
-    //       {/* <button type="button" className="text-slate-100	bg-indigo-500 mb-10 border border-1 border-black sbg-indigo-500">Import Photo Product</button> */}
-    //     </label>
-    //     <input onChange={e => {
-    //       props.setFieldValue("file", e.target.files[0])
-    //     }} style={{ display: "none" }} id='inputFile' type='file' name='inputFile' />
-    //     <span>Name</span>
-    //     <input className="text-center	 mb-6 flex-row border border-1 border-black" onChange={props.handleChange} placeholder="name" type="text" name='name' />
-    //     <span>Description</span>
-    //     <textarea className="text-center	mb-10 flex-row justify-center border border-1 border-black" onChange={props.handleChange} placeholder="description" name="description" />
-    //     <span>Price</span>
-    //     <input className="text-center	mb-6 flex-row justify-center border border-1 border-black" onChange={props.handleChange} placeholder="Price" type="text" name='price' />
-    //     <span>Category</span>
-    //     <input className="text-center	mb-6 flex-row justify-center border border-1 border-black" onChange={props.handleChange} placeholder="Category" type="text" name='category' />
-    //     <button type="submit" className="text-slate-100	bg-indigo-500">Publish</button>
-    //   </form>
-
-    // )}
-    // </Formik>
   );
 }
 
